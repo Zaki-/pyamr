d7a9f75f86a3ff6894e7c1995d4f6fdec5b2de0d
Hussain <Zaki-@users.noreply.github.com>
2016-12-03 11:18:17 -0500

    Update reset_odom.py

--- reset_odom.py
+++ reset_odom.py
@@ -1,5 +1,25 @@
+#!/usr/bin/env python
+import rospy
+import sys
+from std_msgs.msg import Empty
+#sys.path.append("/home/amr")
 
+#import odom_reset
+
+from nav_msgs.msg import Odometry
+def reset():
     pub = rospy.Publisher('/mobile_base/commands/reset_odometry', Empty, queue_size=10)
 
-    for i in range(0,10):
-                pub.publish(Empty())
+#    for i in range(0,10):
+    pub.publish()
+def odometryCb(msg):
+    m = msg.pose.pose.position.x
+    print m
+    if (m > 2):
+        reset()
+
+if __name__ == "__main__":
+    rospy.init_node('odomtest', anonymous=True)  
+    rospy.Subscriber('odom',Odometry,odometryCb)
+
+    rospy.spin()
