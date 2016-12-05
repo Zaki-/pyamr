ee7169a32760ffd3172cd98307c4e35e1c5a9f11
Hussain <Zaki-@users.noreply.github.com>
2016-12-05 11:01:47 -0500

    Update obj_det.py

--- obj_det.py
+++ obj_det.py
@@ -13,11 +13,11 @@ while(1):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # define range of blue color in HSV
-    lower_blue = np.array([110,50,50])
-    upper_blue = np.array([130,255,255])
+    lower_range = np.array([110,50,50])
+    upper_range = np.array([130,255,255])
 
     # Threshold the HSV image to get only blue colors
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    mask = cv2.inRange(hsv, lower_range, upper_range)
 
     # Bitwise-AND mask and original image
     res = cv2.bitwise_and(frame,frame, mask= mask)
